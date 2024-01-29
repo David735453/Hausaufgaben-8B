aa2822af81f320c4ee58dabecba4970f56d15991
David735453 <david.hogea2010@icloud.com>
2024-01-29 15:22:19 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -61,7 +61,7 @@ export default function PostPage({
             <Link href={`/posts/${prevPost.slug}`}>
               <a className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
                 <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
-                  Previous
+                  Vorheriger Tag
                 </p>
                 <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
                   {prevPost.title}
@@ -74,7 +74,7 @@ export default function PostPage({
             <Link href={`/posts/${nextPost.slug}`}>
               <a className="py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
                 <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
-                  Next
+                  Nächster Tag
                 </p>
                 <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
                   {nextPost.title}
