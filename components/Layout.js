37efc80e325973d47f190747d8b86077e094948f
David <david.hogea2010@icloud.com>
2024-09-10 13:00:17 +0200

    verschiedenes

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
-
+import { Analytics } from "@vercel/analytics/react"
 import classNames from 'classnames';
 import { useEffect } from 'react';
 import styles from './Layout.module.css';
