d83b1d52cf1c86d04a18766c4f9906c47677384c
David <david.hogea2010@icloud.com>
2024-09-12 19:27:41 +0200

    vercel features

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 import classNames from 'classnames';
 import { useEffect } from 'react';
