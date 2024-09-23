695ec99165e5ad820b7fac99cf6edce2991fcc8b
David <david.hogea2010@icloud.com>
2024-09-23 15:49:12 +0200

    zu 9B

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Viel Spaß beim Bruch!';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Hausaufgaben 8B';
+    : 'Hausaufgaben 9B';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Weder ich noch jemand der mir etwas zuschickt verspricht, dass diese Angaben auch nur im geringsten richtig sind und über nimmt keine Folgen für diese Fehler.';
